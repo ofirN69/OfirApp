7fb8bd87a39236308ff71c656f399ee50eab0b76
ofirN69 <ofiry.nevo@gmail.com>
2024-12-26 11:01:35 +0200

    innit

---
+++ app/src/main/java/com/ofir/ofirapp/model/Event.java
@@ -0,0 +1,19 @@
+package com.ofir.ofirapp.model;
+
+import java.util.ArrayList;
+
+public class Event {
+    String id;
+    String type;
+    String date;
+    String hour;
+    String venue;
+
+    String address;
+
+    String status;
+
+    ArrayList<String> Menu;
+
+    ArrayList<User> Users;
+}
