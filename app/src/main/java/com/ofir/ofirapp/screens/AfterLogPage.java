8d3603cc867dc928f2ee2720b72a4eb7bb615c61
ofirN69 <ofiry.nevo@gmail.com>
2025-02-20 12:59:26 +0200

    20.2

--- app/src/main/java/com/ofir/ofirapp/screens/AfterLogPage.java
+++ app/src/main/java/com/ofir/ofirapp/screens/AfterLogPage.java
@@ -3,6 +3,7 @@ package com.ofir.ofirapp.screens;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,9 +16,10 @@ import com.ofir.ofirapp.R;
 import com.ofir.ofirapp.models.User;
 import com.ofir.ofirapp.utils.SharedPreferencesUtil;
 
-public class AfterLogPage extends AppCompatActivity {
+public class AfterLogPage extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvHello;
+    Button MyEvents , Add, Info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +30,49 @@ public class AfterLogPage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
 
+        });
+        initviews();
         User user=SharedPreferencesUtil.getUser(this);
 
         tvHello = findViewById(R.id.tv_hello);
-        tvHello.setText(user.getFname() +"היי ");
+        tvHello.setText(user.getFname() + "היי ");
     }
 
+    private void initviews() {
+        MyEvents=findViewById(R.id.btnGoMyEvents);
+        MyEvents.setOnClickListener(this);
+        Add=findViewById(R.id.btnAddEvent);
+        Add.setOnClickListener(this);
+        Info=findViewById(R.id.btnInfo);
+        Info.setOnClickListener(this);
+    }
+
+    @Override
     public void onClick(View view) {
-        Intent goRe = new Intent(getApplicationContext(), AccPage.class);
-        startActivity(goRe);
+        if (view == MyEvents) {
+            Intent goToEvents = new Intent(getApplicationContext(), MyEvents.class);
+            startActivity(goToEvents);
+        }
+        if (view ==Add){
+            Intent goAdd = new Intent(getApplicationContext(), addEVENT.class);
+            startActivity(goAdd);
+
+        }
+        if (view == Info){
+            Intent goInfo = new Intent(getApplicationContext(), AccPage.class);
+            startActivity(goInfo);
+        }
     }
 
 
 
-    public void goMyEvents(View view) {
-        Intent goToEvents = new Intent(getApplicationContext(), MyEvents.class);
-        startActivity(goToEvents);
-    }
+
+
+
+
+
+
+
+
 }
